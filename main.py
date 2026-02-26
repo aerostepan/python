4937bfa7d5771c6be60e53c88aeccc6812f9736f
SoCalledOv <aerostepan@gmail.com>
2026-02-26 13:14:37 +0100

    calc

---
+++ main.py
@@ -0,0 +1,25 @@
+num_a = int(input("Enter the 1st Number: "))
+num_b = int(input("Enter the 2nd Number: "))
+operation = input("Which operation do you want to perform?(*,+,-,/): ")
+
+if operation == "*":
+    suma = num_a * num_b
+elif operation == "+":
+    suma = num_a + num_b
+elif operation == "-":
+    suma = num_a - num_b
+elif operation == "/":
+    if num_a == 0:
+        print("Cannot divide by 0!")
+    else:
+        suma = num_a / num_b
+if operation == "":
+    print("You did not input an operation!")
+    
+
+try:
+    suma
+except NameError:
+    print("Try again")
+else:
+    print(f"Result: {suma}")
