fa735a39e8967dcdf1d86fcac88245eefeae2e41
SoCalledOv <aerostepan@gmail.com>
2026-03-23 23:54:15 +0100

    update

--- practice/gui2.py
+++ practice/gui2.py
@@ -1,13 +1,25 @@
 import sys 
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("My cool GUI")
         self.setGeometry(0,0,500,500)
-        self.setWindowIcon(QIcon("filepath"))
+
+        label = QLabel("Hello!",self)
+        label.setFont(QFont("Arial",40))
+        label.setGeometry(0,0, 500, 100)
+        label.setStyleSheet("color: #E9D2CE;"
+                            "background-color: #440E03;"
+                            "font-weight: bold;"
+                            "font-style: italic;"
+                            "text-decoration: underline;")
+        label.setAlignment(Qt.AlignCenter)#Center & Center HCenter and VCenter
+
 
 
 def main():
